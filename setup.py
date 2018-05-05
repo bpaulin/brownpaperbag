f919997d0e6c60a8f233f5ac15adb93108e4824e
bpaulin <brunopaulin@bpaulin.net>
2018-05-05 10:34:14 +0200

    pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'brownpaperbag',
+  packages = ['brownpaperbag'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'But you try and tell the young people today that... and they won\'t believe ya\'.',
+  author = 'bpaulin',
+  author_email = 'brunopaulin@bpaulin.net',
+  url = 'https://github.com/bpaulin/brownpaperbag', # use the URL to the github repo
+  download_url = 'https://github.com/bpaulin/brownpaperbag/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'openwebnet'], # arbitrary keywords
+  classifiers = [],
+)
