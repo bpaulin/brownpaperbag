14367afaff07e3311fa564db67b8fff7fac2c529
bpaulin <brunopaulin@bpaulin.net>
2019-01-20 21:34:57 +0100

    version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name='brownpaperbag',
   packages=['brownpaperbag'],
-  version='0.0.3',
+  version='0.0.4',
   description='But you try and tell the young people today that... ',
   author='bpaulin',
   author_email='brunopaulin@bpaulin.net',
   url='https://github.com/bpaulin/brownpaperbag',
-  download_url='https://github.com/bpaulin/brownpaperbag/archive/0.0.3.tar.gz',
+  download_url='https://github.com/bpaulin/brownpaperbag/archive/0.0.4.tar.gz',
   keywords=['testing', 'openwebnet'],
   classifiers=[],
 )
