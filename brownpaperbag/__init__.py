07d5801c680b61e8102bc430ed3401fce5c38bb2
bpaulin <brunopaulin@bpaulin.net>
2020-12-15 20:52:52 +0000

    Bump version: 2.0.3 → 2.0.4

--- brownpaperbag/__init__.py
+++ brownpaperbag/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Bruno Paulin"""
 __email__ = "brunopaulin@bpaulin.net"
-__version__ = "2.0.3"
+__version__ = "2.0.4"
 
 # flake8: noqa
 from .bpbgate import BpbCommandSession, BpbEventSession
--- setup.py
+++ setup.py
@@ -29,6 +29,6 @@ setup(
     name="brownpaperbag",
     packages=find_packages(include=["brownpaperbag"]),
     url="https://github.com/bpaulin/brownpaperbag",
-    version="2.0.3",
+    version="2.0.4",
     zip_safe=False,
 )
